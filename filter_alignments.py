94d2cc8797b7a1686b091dea81f334ed0a076936
sromain <sandra.romain@inria.fr>
2022-05-20 18:10:51 +0200

    added license

--- filter_alignments.py
+++ filter_alignments.py
@@ -1,10 +1,28 @@
 #!/usr/bin/env python3
 
+"""*******************************************************************************
+    Name: SVJedi-graph
+    Description: SVjedi-graph aims to genotype structural variant with long reads data using a variation graph.
+    Author: Sandra Romain
+    Contact: sandra.romain@inria.fr, Inria/Univ Rennes/GenScale, Campus de Beaulieu, 35042 Rennes Cedex, France
+    
+    Copyright (C) 2019 Inria
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*******************************************************************************"""
+
 import sys
 import argparse
 import re
 import json
-import statistics as stats
 
 def main(args):
 
